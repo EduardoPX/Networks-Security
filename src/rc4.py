78d7308cbcfdbe1f98a77c5ea9c517370754ca4b
Eduardo De Souza Paixão <duardopx@A204-13L.imd.local>
2018-09-20 17:58:28 -0300

    gg

--- src/rc4.py
+++ src/rc4.py
@@ -1,5 +1,7 @@
 import codecs
-############FIRST STAP##################
+
+
+############FIRST STEP##################
 #!      Encrypting the key
 # Utilize the key schedule algorithm
 # Create a list with 256 positions
@@ -9,7 +11,7 @@ import codecs
 # s -> list contenting [0 ... 255] numbers
 # j -> responsable to store the permutation indice
 
-def key_schedule(key):
+def KSA(key):
     key_length = len(key)
     s = list(range(256))
     j = 0
@@ -43,20 +45,21 @@ def PRGA(s):
 
         yield k
 
-#########THIRD STAP###############################
+#########THIRD STEP###############################
 #!      Prepare the key
-# Apply the functions key_schedule and PRGA
+# Apply the functions KSA and PRGA
 
 def RC4(key):
-    s = key_schedule(key)
+    s = KSA(key)
     return PRGA(s)
 
-#########FOURTH STAP###############################
+#########FOURTH STEP###############################
 #!     Transform string list in integer
 def xor(plainText, key):
     key = [ord(c) for c in key]
     keystream = RC4(key)
     ls = []
+
     for c in plainText:
         conver_xor = ("%02X" % (c ^ next(keystream)))
         ls.append(conver_xor)
@@ -66,8 +69,10 @@ def encrypt(plainText, key):
     plainText = [ord(c) for c in plainText]
     return xor(plainText, key)
 
-def decrypt()
-key = 'Key'
-plainText = 'Plaintext'
+def decrypt(ciphertext, key):
+    ciphertext = codecs.decode(ciphertext, 'hex_codec')
+    res = xor(ciphertext, key)
+    return codecs.decode(res, 'hex_codec').decode('utf-8')
 
-print(encrypt(plainText, key))
+print(encrypt('Plaintext', 'Key'))
+print(decrypt('BBF316E8D940AF0AD3', 'Key')) 
